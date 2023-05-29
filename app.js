194bb6590081e7712d13aff553c517a03216d54f
Mohammed Hijazi <mohammedhijazi199@gmail.com>
2023-05-29 12:44:51 +0300

    use asynchronous

--- app.js
+++ app.js
@@ -1,47 +1,58 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const { User } = require('./database');
-
+const { User } = require("./database");
 
 app.use(express.json());
 
 // CREATE
-app.post('/users', (req, res) => {
-  User.create(req.body)
-    .then(user => res.json(user))
-    .catch(err => res.json(err));
+app.post("/users", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 // READ
-app.get('/users', (req, res) => {
-  User.findAll()
-    .then(users => res.json(users))
-    .catch(err => res.json(err));
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 // UPDATE
-app.put('/users/:id', (req, res) => {
-  User.update(req.body, {
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(result => res.json(result))
-    .catch(err => res.json(err));
+app.put("/users/:id", async (req, res) => {
+  try {
+    const user = await User.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(user);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 // DELETE
-app.delete('/users/:id', (req, res) => {
-  User.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(result => res.json(result))
-    .catch(err => res.json(err));
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(user);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`);
 });
--- database.js
+++ database.js
@@ -18,20 +18,37 @@ const User = sequelize.define('User', {
   // Other model options go here
 });
 
+
 // test the connection
-sequelize.authenticate()
-  .then(() => {
+// sequelize.authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//     // sync the model with the database
+//     User.sync()
+//       .then(() => {
+//         console.log('User table has been synced');
+//       })
+//       .catch(err => console.error('An error occurred while creating the table:', err));
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   });
+
+
+//make the database connection asynchronous
+const setUpDatabase = async () => {
+  try {
+    await sequelize.authenticate();
     console.log('Connection has been established successfully.');
-    // sync the model with the database
-    User.sync()
-      .then(() => {
-        console.log('User table has been synced');
-      })
-      .catch(err => console.error('An error occurred while creating the table:', err));
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+
+    await User.sync();
+    console.log('User table has been synced');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+setUpDatabase();
 
 module.exports = {
   sequelize,
